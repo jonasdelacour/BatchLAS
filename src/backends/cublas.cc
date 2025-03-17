fbc97df6b8842eef580824343552b473892e6f25
jonasdelacour <jonaslacour1@gmail.com>
2025-03-17 12:34:32 +0100

    Added a few backend wrappers to CUBLAS

---
+++ src/backends/cublas.cc
@@ -0,0 +1,114 @@
+#include "../../include/blas/linalg.hh"
+#include "../linalg-impl.hh"
+#include <util/sycl-vector.hh>
+#include <util/sycl-span.hh>
+#include "../queue.hh"
+#include <sycl/sycl.hpp>
+#include <complex>
+
+// This file contains cuBLAS primitives implementation
+namespace batchlas {
+
+    template <Backend B, typename T, BatchType BT>
+    SyclEvent gemm(SyclQueue& ctx,
+                   DenseMatView<T,BT> descrA,
+                   DenseMatView<T,BT> descrB,
+                   DenseMatView<T,BT> descrC,
+                   T alpha,
+                   T beta,
+                   Transpose transA,
+                   Transpose transB,
+                   ComputePrecision precision) {
+        // Call cuBLAS
+        static LinalgHandle<B> handle;
+        handle.setStream(ctx);
+        
+        auto [m, k] = get_effective_dims(descrA, transA);
+        auto [kB, n] = get_effective_dims(descrB, transB);
+
+        if constexpr (BT == BatchType::Single) {
+            cublasGemmEx(handle,
+                enum_convert<BackendLibrary::CUBLAS>(transA), enum_convert<BackendLibrary::CUBLAS>(transB),
+                m, n, k,
+                &alpha,
+                descrA.data_, BackendScalar<T,B>::type, descrA.ld_,
+                descrB.data_, BackendScalar<T,B>::type, descrB.ld_,
+                &beta,
+                descrC.data_, BackendScalar<T,B>::type, descrC.ld_,
+                enum_convert<BackendLibrary::CUBLAS, T>(precision),
+                CUBLAS_GEMM_DFALT);
+        } else {
+            cublasGemmStridedBatchedEx(handle,
+                enum_convert<BackendLibrary::CUBLAS>(transA), enum_convert<BackendLibrary::CUBLAS>(transB),
+                m, n, k,
+                &alpha,
+                descrA.data_, BackendScalar<T,B>::type, descrA.ld_, descrA.stride_,
+                descrB.data_, BackendScalar<T,B>::type, descrB.ld_, descrB.stride_,
+                &beta,
+                descrC.data_, BackendScalar<T,B>::type, descrC.ld_, descrC.stride_,
+                descrA.batch_size_,
+                enum_convert<BackendLibrary::CUBLAS, T>(precision),
+                CUBLAS_GEMM_DFALT);
+        }
+        return ctx.get_event();
+    }
+
+    template <Backend B, typename T, BatchType BT>
+    SyclEvent trsm(SyclQueue& ctx,
+                   DenseMatView<T,BT> descrA,
+                   DenseMatView<T,BT> descrB,
+                   Side side,
+                   Uplo uplo,
+                   Transpose transA,
+                   Diag diag,
+                   T alpha) {
+        static LinalgHandle<B> handle;
+        handle.setStream(ctx);
+        auto [kB, n] = get_effective_dims(descrB, Transpose::NoTrans);
+
+        if constexpr (BT == BatchType::Single) {
+            call_backend<T, BackendLibrary::CUBLAS, B>(cublasStrsm, cublasDtrsm, cublasCtrsm, cublasZtrsm, 
+                handle, side, uplo, transA, diag, kB, n, &alpha, get_data(descrA), descrA.ld_, get_data(descrB), descrB.ld_); 
+        } else {
+            call_backend<T, BackendLibrary::CUBLAS, B>(cublasStrsmBatched, cublasDtrsmBatched, cublasCtrsmBatched, cublasZtrsmBatched, 
+                handle, side, uplo, transA, diag, kB, n, &alpha, get_ptr_arr(ctx, descrA), descrA.ld_, get_ptr_arr(ctx, descrB), descrB.ld_, descrA.batch_size_);
+        }
+        return ctx.get_event();
+    }
+
+    // Template instantiations for cuBLAS functions
+    #define GEMM_INSTANTIATE(fp, BT) \
+    template SyclEvent gemm<Backend::CUDA, fp, BT>( \
+        SyclQueue&, \
+        DenseMatView<fp, BT>, \
+        DenseMatView<fp, BT>, \
+        DenseMatView<fp, BT>, \
+        fp, fp, Transpose, Transpose, ComputePrecision);
+
+    #define TRSM_INSTANTIATE(fp, BT) \
+    template SyclEvent trsm<Backend::CUDA, fp, BT>( \
+        SyclQueue&, \
+        DenseMatView<fp, BT>, \
+        DenseMatView<fp, BT>, \
+        Side, Uplo, Transpose, Diag, fp);
+
+    #define BLAS_LEVEL3_INSTANTIATE(fp, BT) \
+        GEMM_INSTANTIATE(fp, BT) \
+        TRSM_INSTANTIATE(fp, BT)
+
+    // Macro that covers all layout and batch type combinations for a given floating-point type.
+    #define BLAS_LEVEL3_INSTANTIATE_FOR_FP(fp)        \
+        BLAS_LEVEL3_INSTANTIATE(fp, BatchType::Batched) \
+        BLAS_LEVEL3_INSTANTIATE(fp, BatchType::Single)
+
+    // Instantiate for the floating-point types of interest.
+    BLAS_LEVEL3_INSTANTIATE_FOR_FP(float)
+    BLAS_LEVEL3_INSTANTIATE_FOR_FP(double)
+    BLAS_LEVEL3_INSTANTIATE_FOR_FP(std::complex<float>)
+    BLAS_LEVEL3_INSTANTIATE_FOR_FP(std::complex<double>)
+
+    #undef GEMM_INSTANTIATE
+    #undef TRSM_INSTANTIATE
+    #undef BLAS_LEVEL3_INSTANTIATE
+    #undef BLAS_LEVEL3_INSTANTIATE_FOR_FP
+}
