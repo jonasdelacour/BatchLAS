cda8417efb291d00f250d9b628aa168c975b8c31
jonasdelacour <jonaslacour1@gmail.com>
2025-03-17 12:31:35 +0100

    Added convenience header (multiple includes)

---
+++ include/blas/linalg.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+// Main include file for the BatchLAS library that includes all components
+
+// Include the enum declarations
+#include "enums.hh"
+
+// Include matrix and vector handle declarations
+#include "matrix_handle.hh"
+
+// Include function declarations
+#include "functions.hh"
