a4af86b84fb83789a2107a3f2958411380dae491
jonasdelacour <jonaslacour1@gmail.com>
2025-04-02 12:18:34 +0200

    feat: add CMake configuration and Python bindings for BatchLAS

---
+++ python/batchlas/__init__.py
@@ -0,0 +1,10 @@
+"""
+BatchLAS Python bindings.
+
+This package provides Python bindings for the BatchLAS library,
+a batch linear algebra library optimized for GPU acceleration.
+"""
+
+from .batchlas import *
+
+__version__ = "0.1.0"
