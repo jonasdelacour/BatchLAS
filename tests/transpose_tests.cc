eddf195fc861b71130400472ace5de5e53a060a4
jonasdelacour <jonaslacour1@gmail.com>
2025-06-08 19:03:33 +0200

    Add less complicated transpose test

--- tests/transpose_tests.cc
+++ tests/transpose_tests.cc
@@ -28,9 +28,9 @@ TYPED_TEST(TransposeTest, OrthoTransposeIdentity) {
     constexpr int batch_size = 2;
 
     Matrix<T, MatrixFormat::Dense> A = Matrix<T, MatrixFormat::Dense>::Random(m, k, false, batch_size);
-    size_t ws = ortho_buffer_size<Backend::CUDA, T>(*this->ctx, A.view(), Transpose::NoTrans);
+    size_t ws = ortho_buffer_size<Backend::CUDA, T>(*this->ctx, A.view(), Transpose::NoTrans, OrthoAlgorithm::SVQB);
     UnifiedVector<std::byte> workspace(ws);
-    ortho<Backend::CUDA, T>(*this->ctx, A.view(), Transpose::NoTrans, workspace.to_span());
+    ortho<Backend::CUDA, T>(*this->ctx, A.view(), Transpose::NoTrans, workspace.to_span(), OrthoAlgorithm::SVQB);
     this->ctx->wait();
 
     Matrix<T, MatrixFormat::Dense> At = transpose(*this->ctx, A.view());
@@ -50,6 +50,39 @@ TYPED_TEST(TransposeTest, OrthoTransposeIdentity) {
                 T expected = (i == j) ? T(1) : T(0);
                 T val = prod_data[b * stride + j * Prod.ld() + i];
                 ASSERT_NEAR(val, expected, tol);
+            }
+        }
+    }
+}
+
+TYPED_TEST(TransposeTest, SimpleTranspose) {
+    using T = TypeParam;
+    constexpr int m = 8;
+    constexpr int k = 4;
+    constexpr int batch_size = 2;
+
+    Matrix<T, MatrixFormat::Dense> A = Matrix<T, MatrixFormat::Dense>::Random(m, k, false, batch_size);
+    this->ctx->wait();
+
+    Matrix<T, MatrixFormat::Dense> At = transpose(*this->ctx, A.view());
+    this->ctx->wait();
+
+    ASSERT_EQ(At.rows(), k);
+    ASSERT_EQ(At.cols(), m);
+    ASSERT_EQ(At.batch_size(), batch_size);
+
+    auto a_data = A.data();
+    auto at_data = At.data();
+    int a_stride = A.stride();
+    int at_stride = At.stride();
+    T tol = TestFixture::tolerance();
+
+    for (int b = 0; b < batch_size; ++b) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < k; ++j) {
+                T val_a = a_data[b * a_stride + j * A.ld() + i];
+                T val_at = at_data[b * at_stride + i * At.ld() + j];
+                ASSERT_NEAR(val_a, val_at, tol);
             }
         }
     }
