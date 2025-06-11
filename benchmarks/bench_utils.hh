91fc632346cb433384e8e8ddba41f0fc1bf95df8
jonasdelacour <jonaslacour1@gmail.com>
2025-06-11 14:56:18 +0200

    More reasonable sets of sizes to benchmark

--- benchmarks/bench_utils.hh
+++ benchmarks/bench_utils.hh
@@ -4,7 +4,7 @@
 namespace bench_utils {
 
 inline void SquareSizes(benchmark::internal::Benchmark* b) {
-    for (int s : {64, 128, 256, 512, 1024}) {
+    for (int s : {64, 128, 256, 512, 1024, 2048, 4096, 8192}) {
         b->Args({s, s});
     }
 }
@@ -13,13 +13,16 @@ inline void SquareBatchSizes(benchmark::internal::Benchmark* b) {
     for (int s : {64, 128, 256}) {
         b->Args({s, s, 10});
     }
-    for (int s : {64, 128, 256, 512, 1024}) {
+    for (int s : {64, 128, 256, 512}) {
         b->Args({s, s, 100});
+    }
+    for (int s : {64, 128, 256, 512, 1024}) {
+        b->Args({s, s, 1000});
     }
 }
 
 inline void CubeSizes(benchmark::internal::Benchmark* b) {
-    for (int s : {64, 128, 256, 512, 1024}) {
+    for (int s : {64, 128, 256, 512, 1024, 2048, 4096, 8192}) {
         b->Args({s, s, s});
     }
 }
@@ -28,8 +31,11 @@ inline void CubeBatchSizes(benchmark::internal::Benchmark* b) {
     for (int s : {64, 128, 256}) {
         b->Args({s, s, s, 10});
     }
-    for (int s : {64, 128, 256, 512, 1024}) {
+    for (int s : {64, 128, 256, 512}) {
         b->Args({s, s, s, 100});
+    }
+    for (int s : {64, 128, 256, 512, 1024}) {
+        b->Args({s, s, s, 1000});
     }
 }
 
