7288aae1132445f4fc0f8e206c2a254ef3b85374
jonasdelacour <jonaslacour1@gmail.com>
2025-06-26 14:49:06 +0200

    Began implementing chefbd

---
+++ src/extensions/chebfd.cc
@@ -0,0 +1,8 @@
+#include <blas/functions.hh>
+using namespace batchlas;
+
+template <Backend B, MatrixFormat MF, typename T>
+void cheb_fd(const Queue& ctx,
+            const MatrixView<T, MF> A){
+    
+}
