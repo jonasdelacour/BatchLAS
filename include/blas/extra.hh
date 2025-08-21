10485e40886a134f7a0060105a76fa7b6d533971
jonasdelacour <jonaslacour1@gmail.com>
2025-08-21 15:47:55 +0200

    :triangular_ruler: feat(blas): add forwarding overloads for norm and cond functions with Matrix<T, MatrixFormat::Dense>

--- include/blas/extra.hh
+++ include/blas/extra.hh
@@ -13,11 +13,28 @@ namespace batchlas
               const NormType norm_type,
               const Span<float_t<T>> norms);
 
+    // Forwarding overload (owning A)
+    template <typename T, MatrixFormat MF>
+    inline Event norm(Queue &ctx,
+              const Matrix<T, MF> &A,
+              const NormType norm_type,
+              const Span<float_t<T>> norms) {
+        return norm<T,MF>(ctx, MatrixView<T,MF>(A), norm_type, norms);
+    }
+
     // Convenience function which allocates and returns the results stored in an array.
     template <typename T, MatrixFormat MF>
     UnifiedVector<float_t<T>> norm(Queue &ctx,
                           const MatrixView<T, MF> &A,
                           const NormType norm_type = NormType::Frobenius);
+
+    // Forwarding overload (owning A)
+    template <typename T, MatrixFormat MF>
+    inline UnifiedVector<float_t<T>> norm(Queue &ctx,
+                          const Matrix<T, MF> &A,
+                          const NormType norm_type = NormType::Frobenius) {
+        return norm<T,MF>(ctx, MatrixView<T,MF>(A), norm_type);
+    }
 
     //Memory passed from outside
     template <Backend B, typename T, MatrixFormat MF>
@@ -26,6 +43,16 @@ namespace batchlas
               const NormType norm_type,
               const Span<T> conds,
               const Span<std::byte> workspace);
+
+    // Forwarding overload (owning A)
+    template <Backend B, typename T, MatrixFormat MF>
+    inline Event cond(Queue &ctx,
+              const Matrix<T, MF> &A,
+              const NormType norm_type,
+              const Span<T> conds,
+              const Span<std::byte> workspace) {
+        return cond<B,T,MF>(ctx, MatrixView<T,MF>(A), norm_type, conds, workspace);
+    }
 
     size_t cond_buffer_size(Queue &ctx,
                             const MatrixView<float, MatrixFormat::Dense> &A,
@@ -37,15 +64,38 @@ namespace batchlas
                           const MatrixView<T, MF> &A,
                           const NormType norm_type);
 
+    // Forwarding overload (owning A)
+    template <Backend B, typename T, MatrixFormat MF>
+    inline UnifiedVector<T> cond(Queue &ctx,
+                          const Matrix<T, MF> &A,
+                          const NormType norm_type) {
+        return cond<B,T,MF>(ctx, MatrixView<T,MF>(A), norm_type);
+    }
+
     // Batched matrix transpose into preallocated output
     template <typename T, MatrixFormat MF>
     Event transpose(Queue &ctx,
                     const MatrixView<T, MF> &A,
                     const MatrixView<T, MF> &B);
 
+    // Forwarding overload (owning A and B)
+    template <typename T, MatrixFormat MF>
+    inline Event transpose(Queue &ctx,
+                    const Matrix<T, MF> &A,
+                    const Matrix<T, MF> &B) {
+        return transpose<T,MF>(ctx, MatrixView<T,MF>(A), MatrixView<T,MF>(B));
+    }
+
     // Convenience overload allocating the output matrix
     template <typename T, MatrixFormat MF>
     Matrix<T, MF> transpose(Queue &ctx,
                             const MatrixView<T, MF> &A);
+
+    // Forwarding overload (owning A)
+    template <typename T, MatrixFormat MF>
+    inline Matrix<T, MF> transpose(Queue &ctx,
+                            const Matrix<T, MF> &A) {
+        return transpose<T,MF>(ctx, MatrixView<T,MF>(A));
+    }
 
 }
