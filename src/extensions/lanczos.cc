1c44729380a655f2bfe81f7300fbe323306ba640
jonasdelacour <jonaslacour1@gmail.com>
2025-12-17 01:43:05 +0100

    :triangular_ruler: refactor(lanczos): adjust VectorView parameters for new reordering.

--- src/extensions/lanczos.cc
+++ src/extensions/lanczos.cc
@@ -165,31 +165,19 @@ namespace batchlas {
         const auto steqr_params = SteqrParams<T>{32, 10, std::numeric_limits<T>::epsilon(), false, false, true, true, SortOrder::Descending};
         auto steqr_ws = pool.allocate<std::byte>(ctx, steqr_buffer_size<T>(
                                                     ctx, 
-                                                    VectorView<real_t>(alphas.data(), n, 1, n, batch_size),
-                                                    VectorView<real_t>(betas.data(), n-1, 1, n, batch_size),
-                                                    VectorView<real_t>(W.data(), n, 1, n, batch_size),
+                                                    VectorView<real_t>(alphas.data(), n, batch_size),
+                                                    VectorView<real_t>(betas.data(), n-1, batch_size),
+                                                    VectorView<real_t>(W.data(), n, batch_size),
                                                     jobz,
                                                     steqr_params));
         
         init();
         lanczos_iteration(n);
-
-
-        /* tridiagonal_solver<B>(ctx,
-                alphas,
-                betas,
-                W,
-                qr_workspace,
-                jobz,
-                MatrixView<T, MatrixFormat::Dense>(Q_eigenvectors.data(), n, n, n, n*n, batch_size),
-                n,
-                batch_size); */
-        
         
         steqr<B> (ctx,
-            VectorView<real_t>(alphas.data(), n, 1, n, batch_size),
-            VectorView<real_t>(betas.data(), n-1, 1, n, batch_size),
-            VectorView<real_t>(W.data(), n, 1, n, batch_size),
+            VectorView<real_t>(alphas.data(), n, batch_size),
+            VectorView<real_t>(betas.data(), n-1, batch_size),
+            VectorView<real_t>(W.data(), n, batch_size),
             steqr_ws,
             jobz,
             steqr_params,
@@ -242,9 +230,9 @@ namespace batchlas {
         // Additional memory required for the tridiagonal solver
         basic_size += BumpAllocator::allocation_size<std::byte>(ctx,
                         steqr_buffer_size<T>(ctx, 
-                            VectorView<typename base_type<T>::type>(nullptr, n, 1, n, batch_size),
-                            VectorView<typename base_type<T>::type>(nullptr, n-1, 1, n, batch_size),
-                            VectorView<typename base_type<T>::type>(W.data(), n, 1, n, batch_size),
+                            VectorView<typename base_type<T>::type>(nullptr, n, batch_size),
+                            VectorView<typename base_type<T>::type>(nullptr, n-1, batch_size),
+                            VectorView<typename base_type<T>::type>(W.data(), n, batch_size),
                             jobz,
                             SteqrParams<T>{32, 10, std::numeric_limits<T>::epsilon(), false, false, true}));
 
