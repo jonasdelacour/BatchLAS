be03d6ce332b86bf661000c02e3342866f03b21c
jonasdelacour <jonaslacour1@gmail.com>
2026-01-05 13:38:10 +0100

    :sparkles: feat(bench_utils): add support for complex types in benchmark registration macros for CUDA, ROCm, and MKL backends.

--- benchmarks/bench_utils.hh
+++ benchmarks/bench_utils.hh
@@ -1,40 +1,75 @@
 #pragma once
 #include <util/minibench.hh>
 #include <batchlas/backend_config.h>
+#include <complex>
 
 #if BATCHLAS_HAS_CUDA_BACKEND
 #define BATCHLAS_BENCH_CUDA(name, sizes) \
     MINI_BENCHMARK_REGISTER_SIZES((name<float, batchlas::Backend::CUDA>), sizes); \
     MINI_BENCHMARK_REGISTER_SIZES((name<double, batchlas::Backend::CUDA>), sizes);
+
+#define BATCHLAS_BENCH_CUDA_ALL_TYPES(name, sizes) \
+    MINI_BENCHMARK_REGISTER_SIZES((name<float, batchlas::Backend::CUDA>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<double, batchlas::Backend::CUDA>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<std::complex<float>, batchlas::Backend::CUDA>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<std::complex<double>, batchlas::Backend::CUDA>), sizes);
 #else
 #define BATCHLAS_BENCH_CUDA(name, sizes)
+#define BATCHLAS_BENCH_CUDA_ALL_TYPES(name, sizes)
 #endif
 
 #if BATCHLAS_HAS_ROCM_BACKEND
 #define BATCHLAS_BENCH_ROCM(name, sizes) \
     MINI_BENCHMARK_REGISTER_SIZES((name<float, batchlas::Backend::ROCM>), sizes); \
     MINI_BENCHMARK_REGISTER_SIZES((name<double, batchlas::Backend::ROCM>), sizes);
+
+#define BATCHLAS_BENCH_ROCM_ALL_TYPES(name, sizes) \
+    MINI_BENCHMARK_REGISTER_SIZES((name<float, batchlas::Backend::ROCM>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<double, batchlas::Backend::ROCM>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<std::complex<float>, batchlas::Backend::ROCM>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<std::complex<double>, batchlas::Backend::ROCM>), sizes);
 #else
 #define BATCHLAS_BENCH_ROCM(name, sizes)
+#define BATCHLAS_BENCH_ROCM_ALL_TYPES(name, sizes)
 #endif
 
 #if BATCHLAS_HAS_MKL_BACKEND
 #define BATCHLAS_BENCH_MKL(name, sizes) \
     MINI_BENCHMARK_REGISTER_SIZES((name<float, batchlas::Backend::MKL>), sizes); \
     MINI_BENCHMARK_REGISTER_SIZES((name<double, batchlas::Backend::MKL>), sizes);
+
+#define BATCHLAS_BENCH_MKL_ALL_TYPES(name, sizes) \
+    MINI_BENCHMARK_REGISTER_SIZES((name<float, batchlas::Backend::MKL>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<double, batchlas::Backend::MKL>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<std::complex<float>, batchlas::Backend::MKL>), sizes); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<std::complex<double>, batchlas::Backend::MKL>), sizes);
 #else
 #define BATCHLAS_BENCH_MKL(name, sizes)
+#define BATCHLAS_BENCH_MKL_ALL_TYPES(name, sizes)
 #endif
 
 #if BATCHLAS_HAS_HOST_BACKEND
 #define BATCHLAS_BENCH_NETLIB(name, sizes) \
     MINI_BENCHMARK_REGISTER_SIZES((name<float, batchlas::Backend::NETLIB>), sizes##Netlib); \
     MINI_BENCHMARK_REGISTER_SIZES((name<double, batchlas::Backend::NETLIB>), sizes##Netlib);
+
+#define BATCHLAS_BENCH_NETLIB_ALL_TYPES(name, sizes) \
+    MINI_BENCHMARK_REGISTER_SIZES((name<float, batchlas::Backend::NETLIB>), sizes##Netlib); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<double, batchlas::Backend::NETLIB>), sizes##Netlib); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<std::complex<float>, batchlas::Backend::NETLIB>), sizes##Netlib); \
+    MINI_BENCHMARK_REGISTER_SIZES((name<std::complex<double>, batchlas::Backend::NETLIB>), sizes##Netlib);
 #else
 #define BATCHLAS_BENCH_NETLIB(name, sizes)
+#define BATCHLAS_BENCH_NETLIB_ALL_TYPES(name, sizes)
 #endif
 
 #define BATCHLAS_REGISTER_BENCHMARK(name, sizes) \
     BATCHLAS_BENCH_CUDA(name, sizes) \
     BATCHLAS_BENCH_ROCM(name, sizes) \
     BATCHLAS_BENCH_NETLIB(name, sizes)
+
+// Use this only for ops that support complex types.
+#define BATCHLAS_REGISTER_BENCHMARK_ALL_TYPES(name, sizes) \
+    BATCHLAS_BENCH_CUDA_ALL_TYPES(name, sizes) \
+    BATCHLAS_BENCH_ROCM_ALL_TYPES(name, sizes) \
+    BATCHLAS_BENCH_NETLIB_ALL_TYPES(name, sizes)
