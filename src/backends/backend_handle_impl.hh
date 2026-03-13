d18f2770f7076ac966a58de31aaa960ff0cd4619
jonasdelacour <jonaslacour1@gmail.com>
2026-03-13 18:11:45 +0100

    Refactor queue tracing helpers and remove the redundant backend handle source.

--- src/backends/backend_handle_impl.hh
+++ src/backends/backend_handle_impl.hh
@@ -22,8 +22,32 @@ struct BackendMatrixHandle {
     BackendMatrixHandle& operator=(const BackendMatrixHandle&) = default;
     BackendMatrixHandle& operator=(BackendMatrixHandle&&) = default;
 
-    BackendMatrixHandle(const MatrixView<T, MType>& matrix) {
+    BackendMatrixHandle(const MatrixView<T, MType>& matrix) { initialize(matrix); }
+
+    BackendMatrixHandle(const Matrix<T, MType>& matrix) : BackendMatrixHandle(matrix.view()) {}
+
+    #if BATCHLAS_HAS_CUDA_BACKEND
+        constexpr inline operator cusparseDnMatDescr_t() const { return cusparse_descr_; }
+        constexpr inline operator cusparseSpMatDescr_t() const { return cusparse_descr_sp_; }
+    #endif
+
+    #if BATCHLAS_HAS_ROCM_BACKEND
+        constexpr inline operator rocsparse_dnmat_descr() const { return rocsparse_descr_; }
+        constexpr inline operator rocsparse_spmat_descr() const { return rocsparse_descr_sp_; }
+    #endif
+
+private:
+    void initialize(const MatrixView<T, MType>& matrix) {
         #if BATCHLAS_HAS_CUDA_BACKEND
+            initialize_cuda_descriptors(matrix);
+        #endif
+        #if BATCHLAS_HAS_ROCM_BACKEND
+            initialize_rocm_descriptors(matrix);
+        #endif
+    }
+
+    #if BATCHLAS_HAS_CUDA_BACKEND
+        void initialize_cuda_descriptors(const MatrixView<T, MType>& matrix) {
             if constexpr (MType == MatrixFormat::Dense && is_complex_or_floating_point<T>::value) {
                 cusparseCreateDnMat(&cusparse_descr_, matrix.rows(), matrix.cols(), matrix.ld(), matrix.data_ptr(), BackendScalar<T, BackendLibrary::CUSPARSE>::type, CUSPARSE_ORDER_COL);
                 if (matrix.batch_size() > 1) {
@@ -36,14 +60,29 @@ struct BackendMatrixHandle {
                     cusparseCsrSetStridedBatch(cusparse_descr_sp_, matrix.batch_size(), matrix.offset_stride(), matrix.matrix_stride());
                 }
             }
-        #endif
-        #if BATCHLAS_HAS_ROCM_BACKEND
+        }
+
+        cusparseDnMatDescr_t cusparse_descr_ = nullptr;
+        cusparseSpMatDescr_t cusparse_descr_sp_ = nullptr;
+    #endif
+
+    #if BATCHLAS_HAS_ROCM_BACKEND
+        static constexpr rocsparse_datatype rocsparse_value_type() {
+            if constexpr (std::is_same_v<T, float>) {
+                return rocsparse_datatype_f32_r;
+            } else if constexpr (std::is_same_v<T, double>) {
+                return rocsparse_datatype_f64_r;
+            } else if constexpr (std::is_same_v<T, std::complex<float>>) {
+                return rocsparse_datatype_f32_c;
+            } else {
+                return rocsparse_datatype_f64_c;
+            }
+        }
+
+        void initialize_rocm_descriptors(const MatrixView<T, MType>& matrix) {
             if constexpr (MType == MatrixFormat::Dense && is_complex_or_floating_point<T>::value) {
                 rocsparse_create_dnmat_descr(&rocsparse_descr_, matrix.rows(), matrix.cols(), matrix.ld(), matrix.data_ptr(),
-                                             std::is_same_v<T, float> ? rocsparse_datatype_f32_r :
-                                             std::is_same_v<T, double> ? rocsparse_datatype_f64_r :
-                                             std::is_same_v<T, std::complex<float>> ? rocsparse_datatype_f32_c :
-                                             rocsparse_datatype_f64_c,
+                                             rocsparse_value_type(),
                                              rocsparse_order_column);
                 if (matrix.batch_size() > 1) {
                     rocsparse_dnmat_set_strided_batch(rocsparse_descr_, matrix.batch_size(), matrix.stride());
@@ -52,33 +91,15 @@ struct BackendMatrixHandle {
                 rocsparse_create_csr_descr(&rocsparse_descr_sp_, matrix.rows(), matrix.cols(), matrix.nnz(),
                                            matrix.row_offsets().data(), matrix.col_indices().data(), matrix.data_ptr(),
                                            rocsparse_indextype_i32, rocsparse_indextype_i32, rocsparse_index_base_zero,
-                                           std::is_same_v<T, float> ? rocsparse_datatype_f32_r :
-                                           std::is_same_v<T, double> ? rocsparse_datatype_f64_r :
-                                           std::is_same_v<T, std::complex<float>> ? rocsparse_datatype_f32_c :
-                                           rocsparse_datatype_f64_c);
+                                           rocsparse_value_type());
                 if (matrix.batch_size() > 1) {
                     rocsparse_csr_set_strided_batch(rocsparse_descr_sp_, matrix.batch_size(), matrix.offset_stride(), matrix.matrix_stride());
                 }
             }
-        #endif
-    }
+        }
 
-    BackendMatrixHandle(const Matrix<T, MType>& matrix) : BackendMatrixHandle(matrix.view()) {}
-
-    #if BATCHLAS_HAS_CUDA_BACKEND
-        cusparseDnMatDescr_t cusparse_descr_ = nullptr;
-        cusparseSpMatDescr_t cusparse_descr_sp_ = nullptr;
-
-        constexpr inline operator cusparseDnMatDescr_t() const { return cusparse_descr_; }
-        constexpr inline operator cusparseSpMatDescr_t() const { return cusparse_descr_sp_; }
-    #endif
-
-    #if BATCHLAS_HAS_ROCM_BACKEND
         rocsparse_dnmat_descr rocsparse_descr_ = nullptr;
         rocsparse_spmat_descr rocsparse_descr_sp_ = nullptr;
-
-        constexpr inline operator rocsparse_dnmat_descr() const { return rocsparse_descr_; }
-        constexpr inline operator rocsparse_spmat_descr() const { return rocsparse_descr_sp_; }
     #endif
 };
 
--- src/queue.hh
+++ src/queue.hh
@@ -5,7 +5,10 @@
 #include <cstddef>
 #include <cstdlib>
 
+#include <atomic>
 #include <mutex>
+#include <optional>
+#include <string>
 #include <unordered_map>
 
 #include "util/kernel-trace.hh"
@@ -51,144 +54,127 @@ struct QueueImpl : public sycl::queue{
                 sycl::device::get_devices(sycl::info::device_type::host)};
 
     static_assert(device_arrays.size() == (int)DeviceType::NUM_DEV_TYPES && "DeviceType enum does not match device_arrays size");
+
+    static sycl::property_list make_queue_properties(bool in_order) {
+        const bool profiling_enabled = batchlas_queue_profiling_enabled();
+        if (in_order && profiling_enabled) {
+            return sycl::property_list{sycl::property::queue::in_order{},
+                                       sycl::property::queue::enable_profiling{}};
+        }
+        if (in_order) {
+            return sycl::property_list{sycl::property::queue::in_order{}};
+        }
+        if (profiling_enabled) {
+            return sycl::property_list{sycl::property::queue::enable_profiling{}};
+        }
+        return sycl::property_list{};
+    }
+
+    static std::uint32_t allocate_trace_tid() {
+        return batchlas_kernel_trace::enabled() ? ++trace_tid_counter_ : 0;
+    }
+
+    static const char* trace_label_or_default(const char* default_label) {
+        const char* scope = batchlas_kernel_trace::current_scope_name();
+        return scope ? scope : default_label;
+    }
+
+    template <typename SubmitOp>
+    sycl::event submit_and_record(const char* default_label, SubmitOp&& submit_op) {
+        sycl::event event = std::forward<SubmitOp>(submit_op)();
+        last_event_ = event;
+        batchlas_kernel_trace::record_event(*this, event, trace_label_or_default(default_label), trace_tid_);
+        return event;
+    }
     
     QueueImpl(Device dev, bool in_order)
         : sycl::queue(shared_context(dev),
                       device_arrays.at((int)dev.type).at(dev.idx),
-                      [&] {
-                          const bool prof = batchlas_queue_profiling_enabled();
-                          if (in_order && prof)
-                              return sycl::property_list{sycl::property::queue::in_order{},
-                                                        sycl::property::queue::enable_profiling{}};
-                          if (in_order && !prof)
-                              return sycl::property_list{sycl::property::queue::in_order{}};
-                          if (!in_order && prof)
-                              return sycl::property_list{sycl::property::queue::enable_profiling{}};
-                          return sycl::property_list{};
-                      }()),
+                      make_queue_properties(in_order)),
           device_(dev),
-          trace_tid_(batchlas_kernel_trace::enabled() ? ++trace_tid_counter_ : 0) {}
+          trace_tid_(allocate_trace_tid()) {}
 
     QueueImpl(const sycl::context& ctx, const sycl::device& dev, Device logical_dev, bool in_order)
         : sycl::queue(ctx,
                       dev,
-                      [&] {
-                          const bool prof = batchlas_queue_profiling_enabled();
-                          if (in_order && prof)
-                              return sycl::property_list{sycl::property::queue::in_order{},
-                                                        sycl::property::queue::enable_profiling{}};
-                          if (in_order && !prof)
-                              return sycl::property_list{sycl::property::queue::in_order{}};
-                          if (!in_order && prof)
-                              return sycl::property_list{sycl::property::queue::enable_profiling{}};
-                          return sycl::property_list{};
-                      }()),
+                      make_queue_properties(in_order)),
           device_(logical_dev),
-          trace_tid_(batchlas_kernel_trace::enabled() ? ++trace_tid_counter_ : 0) {}
+          trace_tid_(allocate_trace_tid()) {}
 
     QueueImpl()
         : sycl::queue(shared_context(Device{0, DeviceType::CPU}),
                       device_arrays.at((int)DeviceType::CPU).at(0),
-                      batchlas_queue_profiling_enabled()
-                          ? sycl::property_list{sycl::property::queue::enable_profiling{}}
-                          : sycl::property_list{}),
+                      make_queue_properties(false)),
           device_(Device{0, DeviceType::CPU}),
-          trace_tid_(batchlas_kernel_trace::enabled() ? ++trace_tid_counter_ : 0) {}
+          trace_tid_(allocate_trace_tid()) {}
 
     template <typename SubmitFunc>
     sycl::event submit(SubmitFunc&& f) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_submit";
-        sycl::event e = sycl::queue::submit(std::forward<SubmitFunc>(f));
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
+        return submit_and_record("sycl_submit", [&] {
+            return sycl::queue::submit(std::forward<SubmitFunc>(f));
+        });
     }
 
     template <int Dimensions, typename KernelFunc>
     sycl::event parallel_for(const sycl::range<Dimensions>& num_work_items, KernelFunc&& kernel_func) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_parallel_for";
-        sycl::event e = sycl::queue::parallel_for(num_work_items, std::forward<KernelFunc>(kernel_func));
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
+        return submit_and_record("sycl_parallel_for", [&] {
+            return sycl::queue::parallel_for(num_work_items, std::forward<KernelFunc>(kernel_func));
+        });
     }
 
     template <typename KernelFunc>
     sycl::event parallel_for(std::size_t num_work_items, KernelFunc&& kernel_func) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_parallel_for";
         auto kfunc = std::forward<KernelFunc>(kernel_func);
-        sycl::event e = sycl::queue::parallel_for(sycl::range<1>(num_work_items), [=](sycl::id<1> idx) {
-            kfunc(static_cast<std::size_t>(idx[0]));
+        return submit_and_record("sycl_parallel_for", [&] {
+            return sycl::queue::parallel_for(sycl::range<1>(num_work_items), [=](sycl::id<1> idx) {
+                kfunc(static_cast<std::size_t>(idx[0]));
+            });
         });
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
     }
 
     template <int Dimensions, typename KernelFunc>
     sycl::event parallel_for(const sycl::nd_range<Dimensions>& exec_range, KernelFunc&& kernel_func) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_parallel_for";
-        sycl::event e = sycl::queue::parallel_for(exec_range, std::forward<KernelFunc>(kernel_func));
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
+        return submit_and_record("sycl_parallel_for", [&] {
+            return sycl::queue::parallel_for(exec_range, std::forward<KernelFunc>(kernel_func));
+        });
     }
 
     template <typename KernelName, int Dimensions, typename KernelFunc>
     sycl::event parallel_for(const sycl::range<Dimensions>& num_work_items, KernelFunc&& kernel_func) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_parallel_for";
-        sycl::event e = sycl::queue::parallel_for<KernelName>(num_work_items, std::forward<KernelFunc>(kernel_func));
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
+        return submit_and_record("sycl_parallel_for", [&] {
+            return sycl::queue::parallel_for<KernelName>(num_work_items, std::forward<KernelFunc>(kernel_func));
+        });
     }
 
     template <typename KernelName, typename KernelFunc>
     sycl::event parallel_for(std::size_t num_work_items, KernelFunc&& kernel_func) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_parallel_for";
         auto kfunc = std::forward<KernelFunc>(kernel_func);
-        sycl::event e = sycl::queue::parallel_for<KernelName>(sycl::range<1>(num_work_items), [=](sycl::id<1> idx) {
-            kfunc(static_cast<std::size_t>(idx[0]));
+        return submit_and_record("sycl_parallel_for", [&] {
+            return sycl::queue::parallel_for<KernelName>(sycl::range<1>(num_work_items), [=](sycl::id<1> idx) {
+                kfunc(static_cast<std::size_t>(idx[0]));
+            });
         });
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
     }
 
     template <typename KernelName, int Dimensions, typename KernelFunc>
     sycl::event parallel_for(const sycl::nd_range<Dimensions>& exec_range, KernelFunc&& kernel_func) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_parallel_for";
-        sycl::event e = sycl::queue::parallel_for<KernelName>(exec_range, std::forward<KernelFunc>(kernel_func));
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
+        return submit_and_record("sycl_parallel_for", [&] {
+            return sycl::queue::parallel_for<KernelName>(exec_range, std::forward<KernelFunc>(kernel_func));
+        });
     }
 
     template <typename KernelFunc>
     sycl::event single_task(KernelFunc&& kernel_func) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_single_task";
-        sycl::event e = sycl::queue::single_task(std::forward<KernelFunc>(kernel_func));
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
+        return submit_and_record("sycl_single_task", [&] {
+            return sycl::queue::single_task(std::forward<KernelFunc>(kernel_func));
+        });
     }
 
     template <typename KernelName, typename KernelFunc>
     sycl::event single_task(KernelFunc&& kernel_func) {
-        const char* scope = batchlas_kernel_trace::current_scope_name();
-        const char* label = scope ? scope : "sycl_single_task";
-        sycl::event e = sycl::queue::single_task<KernelName>(std::forward<KernelFunc>(kernel_func));
-        last_event_ = e;
-        batchlas_kernel_trace::record_event(*this, e, label, trace_tid_);
-        return e;
+        return submit_and_record("sycl_single_task", [&] {
+            return sycl::queue::single_task<KernelName>(std::forward<KernelFunc>(kernel_func));
+        });
     }
 
     const Device device_;
